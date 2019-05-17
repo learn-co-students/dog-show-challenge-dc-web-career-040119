8b019eb0e277d3279da1483da73100a34c9dc712
github username <github email address>
2019-05-17 12:36:20 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,98 @@
 document.addEventListener('DOMContentLoaded', () => {
+    throwTheBall()
 
+    document.getElementById('dog-form').addEventListener('submit', haircut)
 })
+
+//FETCH!!
+const throwTheBall = () => {
+    fetch('http://localhost:3000/dogs')
+        .then(response => response.json())
+        .then(goodDogs => goodDogs.forEach(pup => giveCookiesTo(pup)))
+}
+
+//GOOD DOG!!
+const giveCookiesTo = (doggo) => {
+    const dogPile = document.getElementById('table-body')
+
+    const goodDog = document.createElement('tr')
+    goodDog.dataset.dogId = doggo.id
+    dogPile.appendChild(goodDog)
+
+    const dogName = document.createElement('td')
+    dogName.innerText = doggo.name
+    goodDog.appendChild(dogName)
+
+    const dogBreed = document.createElement('td')
+    dogBreed.innerText = doggo.breed
+    goodDog.appendChild(dogBreed)
+
+    const dogSex = document.createElement('td')
+    dogSex.innerText = doggo.sex
+    goodDog.appendChild(dogSex)
+
+    const editField = document.createElement('td')
+    const editButton = document.createElement('button')
+    editButton.innerText = 'Edit Dog'
+    editButton.addEventListener('click', takeDogToTheGroomers)
+    editField.appendChild(editButton)
+    goodDog.appendChild(editField)
+}
+
+const takeDogToTheGroomers = e => {
+
+    const editForm = document.getElementById('dog-form')
+    const dogSex = e.target.parentElement.previousSibling
+    const dogBreed = dogSex.previousSibling
+    const dogName = dogBreed.previousSibling
+    const dogId = dogName.parentElement.dataset.dogId
+
+    // editForm.removeEventListener("submit", haircut)
+
+    editForm.dataset.editDogId = dogId
+    editForm[0].value = dogName.innerText
+    editForm[1].value = dogBreed.innerText
+    editForm[2].value = dogSex.innerText
+
+}
+
+const haircut = (e) => {
+    e.preventDefault()
+
+    const name = e.target.children[0].value
+    const breed = e.target.children[1].value
+    const sex = e.target.children[2].value
+
+    if (!!name && !!breed) {
+        if (sex === "male" || sex === "female") {
+
+            fetch(`http://localhost:3000/dogs/${e.target.dataset.editDogId}`, {
+                method: 'PATCH',
+                headers: {
+                    "Content-Type": "application/json",
+                    "Accept": "application/json"
+                },
+                body: JSON.stringify({
+                    name: e.target.children[0].value,
+                    breed: e.target.children[1].value,
+                    sex: e.target.children[2].value
+                })
+            })
+                .then(response => response.json())
+                .then(updatePrettyPup)
+            e.target.dataset.editDogId = ''
+            e.target.reset()
+        } else {
+            alert('Sex must be either "male" or "female"')
+        }
+    } else {
+        alert('Dog must have a name and a breed')
+    }
+}
+
+const updatePrettyPup = (pup) => {
+    const prettyDoggie = document.querySelector(`[data-dog-id = "${pup.id}"]`)
+    prettyDoggie.children[0].innerText = pup.name
+    prettyDoggie.children[1].innerText = pup.breed
+    prettyDoggie.children[2].innerText = pup.sex
+}
